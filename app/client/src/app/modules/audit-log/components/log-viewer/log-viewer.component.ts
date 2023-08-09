263d7620966efe40e1f5fd09acb614e0f5033549
Shafiul Bashar <dshafiul@gmail.com>
2023-08-09 23:54:29 +0600

    audit log module and component added

---
+++ app/client/src/app/modules/audit-log/components/log-viewer/log-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-viewer',
+  templateUrl: './log-viewer.component.html',
+  styleUrls: ['./log-viewer.component.scss']
+})
+export class LogViewerComponent {
+
+}
