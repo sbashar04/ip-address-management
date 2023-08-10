db93a4ce8065f807bddd3aaf327d219a2e5a8d1e
Shafiul Bashar <dshafiul@gmail.com>
2023-08-11 01:10:12 +0600

    reactivity added in login form

--- app/client/src/app/modules/login/components/login/login.component.ts
+++ app/client/src/app/modules/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      remember: [false],
+    });
+  }
+
+  handleLoginFormSubmission () {
+    console.log(this.form.value);
+  }
 
 }
--- app/client/src/app/modules/login/login.module.ts
+++ app/client/src/app/modules/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -17,6 +18,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    ReactiveFormsModule,
   ]
 })
 export class LoginModule { }
