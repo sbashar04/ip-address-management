81aaa201ef09cb3d9dbf43750a85bf1b7db72ed2
Shafiul Bashar <dshafiul@gmail.com>
2023-08-10 22:42:37 +0600

    angular app running by the docker and changes effects

--- app/client/src/app/modules/login/services/login.service.ts
+++ app/client/src/app/modules/login/services/login.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  login(model: any): Observable<any> {
+    return this.http.post('', model);
+  }
+
 }
