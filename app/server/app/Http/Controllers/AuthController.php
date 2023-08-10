a7001dcdf6b48a959083c26f06945c9b64a0fb04
Shafiul Bashar <dshafiul@gmail.com>
2023-08-11 01:01:23 +0600

    auth related apis created and ip address model controller and migrations created

---
+++ app/server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $response = ['success' => false];
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails())
+        {
+            $response['errors'] = $validator->errors()->all();
+            return response()->json($response, 422);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (auth()->attempt($credentials)) {
+            $user = Auth::user();
+            $user['token'] = $user->createToken('Laravel Passport Personal Token')->accessToken;
+            $response['success'] = true;
+            $response['data']['user'] = $user;
+            return response()->json($response, 200);
+        }
+        $response['errors']['message'] = 'Invalid credentials';
+        return response()->json($response, 402);
+    }
+
+    public function register(Request $request)
+    {
+        $response = ['success' => false];
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails())
+        {
+            $response['errors'] = $validator->errors()->all();
+            return response()->json($response, 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $response['success'] = true;
+        $response['data']['user'] = $user;
+        return response()->json($response);
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $token = $request->user()->token();
+            $token->revoke();
+            return response()->json(['success' => true, 'message' => 'Logout successful!'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => 'Request was unsuccessful.'], 422);
+        }
+    }
+}
--- app/server/routes/api.php
+++ app/server/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IpAddressController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => '/auth'], function(){
+    Route::group(['middleware' => ['auth:api']], function(){
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+Route::apiResources([
+    'ip-address' => IpAddressController::class
+]);
