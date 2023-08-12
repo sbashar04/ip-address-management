f87bdad8c21752ef4719c0979a4bf2f16b78e327
Shafiul Bashar <dshafiul@gmail.com>
2023-08-12 09:37:36 +0600

    sidebar component added

--- app/client/src/app/app.module.ts
+++ app/client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,8 @@ import { HeaderComponent } from './components/header/header.component';
     AppRoutingModule,
     HttpClientModule,
     HeaderComponent,
+    SidebarComponent,
+  ],
   ],
   providers: [],
   bootstrap: [AppComponent]
