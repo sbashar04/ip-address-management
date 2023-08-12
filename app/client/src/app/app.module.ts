7477926dd4b29c0ad1a0fbd6eeb0736bab0d9764
Shafiul Bashar <dshafiul@gmail.com>
2023-08-12 08:43:12 +0600

    header component separated

--- app/client/src/app/app.module.ts
+++ app/client/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/client/src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  logout(){
+    this.router.navigate(['login']);
+  }
+
+}
