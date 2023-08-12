e3ad625cb873971494a32c8a7c842d02a40dab82
Shafiul Bashar <dshafiul@gmail.com>
2023-08-12 14:48:43 +0600

    interceptor update

--- app/client/src/app/interceptors/http-handler.interceptor.ts
+++ app/client/src/app/interceptors/http-handler.interceptor.ts
@@ -10,34 +10,48 @@ import { Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { endpointsWithoutAuth } from '../config/constants';
+import { AuthService } from '../services/auth/auth.service';
 
 @Injectable()
 export class HttpHandlerInterceptor implements HttpInterceptor {
 
   constructor(
     private router: Router,
+    private authService: AuthService,
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     // Retrieving the token
-    const token = '';
-    console.log(request.url);
-    request = request.clone({url: `${environment.apiUrl}${request.url}`});
-    request.headers.set('Content-Type', 'application/json');
+    const token = this.authService.getToken();
+
+    let headers = {}
+
+    headers = {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Accept': 'application/json',
+    }
 
     // Ignoring authorization for some endpoints
     if(!endpointsWithoutAuth.includes(request.url)){
-      request.headers.set('Authorization', `Bearer ${token}`);
+      headers = {
+        ...headers,
+        'Authorization': `Bearer ${token}`,
+      }
     }
+
+    request = request.clone({
+      url: `${environment.apiUrl}${request.url}`,
+      setHeaders: headers
+    });
 
     return next.handle(request).pipe(
       tap({
         next: (event) => {
           if (event instanceof HttpResponse) {
             if (event.status == 401) {
-              alert('unauthenticated');
-              this.router.navigate(['login']);
+              // this.authService.logout();
+              // this.router.navigate(['login']);
 
               // @TODO Add toast service
             }
@@ -46,7 +60,7 @@ export class HttpHandlerInterceptor implements HttpInterceptor {
         },
         error: (error) => {
           if(error.status === 401) {
-            alert('unauthenticated');
+            this.authService.logout();
             this.router.navigate(['login']);
             // @TODO Add toast service
           } else if(error.status === 404) {
@@ -55,14 +69,5 @@ export class HttpHandlerInterceptor implements HttpInterceptor {
         }
       })
     );
-
-    // .pipe(
-    //   catchError((requestError: any): any => {
-    //     if (requestError.status !== 401) {
-    //       this.router.navigate(['login']);
-    //       // @TODO Add toast service
-    //     }
-    //   })
-    // );
   }
 }
