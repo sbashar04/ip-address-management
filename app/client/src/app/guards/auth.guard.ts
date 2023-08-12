b47d29f9512fb4efe33b555ab30e89e52f1b72df
Shafiul Bashar <dshafiul@gmail.com>
2023-08-12 09:35:59 +0600

    auth guard update

--- app/client/src/app/guards/auth.guard.ts
+++ app/client/src/app/guards/auth.guard.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+export class AuthGuard {
+
+  router = inject(Router);
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = 'yes';
+    if (!token) {
+      this.router.navigate(['login']);
+      return false;
+    }
     return true;
   }
-  
+
 }
