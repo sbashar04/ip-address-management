6011fb2cfb2444e1534716dd660791a6e9cafff5
Shafiul Bashar <dshafiul@gmail.com>
2023-08-15 20:14:33 +0600

    audit log related pagination

--- app/client/src/app/modules/audit-log/audit-log.module.ts
+++ app/client/src/app/modules/audit-log/audit-log.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogViewerComponent } from './components/log-viewer/log-viewer.component';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginationComponent } from 'src/app/components/pagination/pagination.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    PaginationComponent,
   ]
 })
 export class AuditLogModule { }
